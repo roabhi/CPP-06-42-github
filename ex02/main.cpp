979df965763396314054865d3bdaec61fa207d6b
Roberto Abril hidalgo <rabril-h@car15s1.42barcelona.com>
2023-11-06 18:14:40 +0100

    Edited ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rabril-h <rabril-h@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 17:38:37 by rabril-h          #+#    #+#             */
-/*   Updated: 2023/11/06 17:56:35 by rabril-h         ###   ########.fr       */
+/*   Updated: 2023/11/06 18:14:14 by rabril-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,38 +21,29 @@
 
 Base * generate(void)
 {
-	int	randomNumber = rand() % 3;
+	int	randomNumber = rand() % 4;
+  //int	randomNumber = rand() % 3;
 
-	switch (randomNumber)
-	{
-		case 0:
-			return new A();
-		case 1:
-			return new B();
-		case 2:
-			return new C();
-	}
-	return NULL;
+  if (randomNumber == 0)
+    return new A();
+  else if (randomNumber == 1)
+    return new B();
+  else if (randomNumber == 2)
+    return new C();
+  else
+	  return NULL;
 }
 
 void identify(Base* ptr)
 {
 	if (ptr == nullptr)
-	{
 		std::cout << "NULL pointer" << std::endl;
-	}
 	else if (dynamic_cast<A*>(ptr))
-	{
-		std::cout << "Clase A pointer" << std::endl;
-	}
+		std::cout << "Pointer of type Class A" << std::endl;
 	else if (dynamic_cast<B*>(ptr))
-	{
-		std::cout << "Clase B pointer" << std::endl;
-	}
+		std::cout << "Pointer of type Class B" << std::endl;
 	else if (dynamic_cast<C*>(ptr))
-	{
-		std::cout << "Clase C pointer" << std::endl;
-	}
+		std::cout << "Pointer of type Class C" << std::endl;
 }
 
 void identify(Base& p)
@@ -64,40 +55,60 @@ int main(void)
 {
   std::srand(std::time(0));
 
+  std::cout << "Instance a has randomly being created as " << std::endl;
+
 	Base *a = generate();
 	identify(*a);
 	identify(a);
 	std::cout << std::endl;
+
+  std::cout << "Instance b has randomly being created as " << std::endl;
+
 	Base *b = generate();
 	identify(*b);
 	identify(b);
 	std::cout << std::endl;
+
+  std::cout << "Instance c has randomly being created as " << std::endl;
+
 	Base *c = generate();
 	identify(*c);
 	identify(c);
 	std::cout << std::endl;
+
+  std::cout << "Instance d has randomly being created as " << std::endl;
+
+
 	Base *d = generate();
 	identify(*d);
 	identify(d);
 	std::cout << std::endl;
+
+  std::cout << "Instance e has randomly being created as " << std::endl;
+
   Base *e = generate();
 	identify(*e);
 	identify(e);
 	std::cout << std::endl;
-  Base *f = generate();
-	identify(*f);
-	identify(f);
-	std::cout << std::endl;
-		
 
-	Base *z = NULL;
-	identify(z);
-	identify(*z);
+  //std::cout << "Instance f has randomly being created as " << std::endl;
+
+
+  // Base *f = generate();
+	// identify(*f);
+	// identify(f);
+	// std::cout << std::endl;
+	
+  // std::cout << "Instance z is a NULL pointer and it has been identified as  " << std::endl;
+
+	// Base *z = NULL;
+	// identify(z);
+	// identify(*z);
 	delete	a;
 	delete	b;
 	delete	c;
 	delete	d;
   delete  e;
-  delete  f;
+  //delete  f;
 	return ( 0 );
 }
